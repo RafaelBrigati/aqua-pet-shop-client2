d2d7ee406f9d3de1a49d79104fc07e82f2efba53
Rafael Brigati <johndoe@example.comgit config --global user.name Rafael>
2023-09-15 23:22:55 +0200

    Deployment

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
  
 const api = axios.create({
-  baseURL: "http://localhost:5005"
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 const signUp = ({ username, password, email }) => {
